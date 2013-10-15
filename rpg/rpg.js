e1a21eeaa0214d9e1aa2d0bc5261c4119ea6dfaa
frodosamoa <andrewrkowalczyk@gmail.com>
2013-10-15 13:48:25 -0700

    attempting glyphicon color change

--- rpg/rpg.js
+++ rpg/rpg.js
@@ -3,21 +3,21 @@ $(function () {
 	// TODO:
 	// Edit modal and delete modal unclickable if no characters.
 	// Create and delete modal unclickable when editing character.
-	// If cahracter is clicked, create character is unclickable.
+	// If character is clicked, create character is unclickable.
 
 
 	$("#confirm-create-button").click(function () {
-		console.log("Character created.");
-        $('#createModal').modal('hide');
+        $("#edit-button").removeAttr("disabled");
+        $("#delete-button").removeAttr("disabled");
+		$('#edit-glyph.glyphicon glyphicon-edit black').removeClass('glyphicon glyphicon-edit black').addClass('glyphicon glyphicon-edit white');
+		$('#createModal').modal('hide');
     });
 
     $("#confirm-edit-button").click(function () {
-    	console.log("Character edited.");
         $('#editModal').modal('hide');
     });
 
     $("#confirm-delete-button").click(function () {
-    	console.log("Character delted.");
         $('#deleteModal').modal('hide');
     });
 
