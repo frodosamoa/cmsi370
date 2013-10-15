9241d12932b5dfdddc7a8db8a87d56b7069c374f
frodosamoa <andrewrkowalczyk@gmail.com>
2013-10-14 21:25:48 -0700

    added small todo list

--- rpg/rpg.js
+++ rpg/rpg.js
@@ -1,5 +1,11 @@
 $(function () {
 
+	// TODO:
+	// Edit modal and delete modal unclickable if no characters.
+	// Create and delete modal unclickable when editing character.
+	// If cahracter is clicked, create character is unclickable.
+
+
 	$("#confirm-create-button").click(function () {
 		console.log("Character created.");
         $('#createModal').modal('hide');
