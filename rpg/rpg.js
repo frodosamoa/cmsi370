031fb403b6061d08154b958e103c29abb6384dd4
frodosamoa <andrewrkowalczyk@gmail.com>
2013-10-23 02:21:01 -0700

    moved around code for better logic; figured out how to get a character's info to the info table; added AJAX code for character creation, edition, and deletion; removed detailed info div code

--- rpg/rpg.js
+++ rpg/rpg.js
@@ -10,34 +10,115 @@ $(function () {
 
 
     var characterInfoRowTemplate = '<tr>' +
-        '<td><strong>Class</strong></td>' + 
-        '<td></td>' +
+        '<td><strong>Class</strong></td>' +
+        '<td id="character-class"></td>' +
         '</tr>' +
         '<tr>' +
         '<td><strong>Gender</strong></td>' +
-        '<td></td>' + 
+        '<td id="character-gender"></td>' +
         '</tr>' + 
         '<tr>' +
         '<td><strong>Level</strong></td>' +
-        '<td></td>' +
+        '<td id="character-level"></td>' +
         '</tr>' +
         '<tr>' +
         '<td><strong>Money</strong></td>' +
-        '<td></td>' +
+        '<td id="character-money"></td>' +
         '</tr>';
 
     // Character Creation
 	$("#confirm-create-button").click(function () {
-        // Add row to character list.
-        $("#character-list-table").append("<tr><td>" + $("#character-name-input").val() + "</td></tr>");
-
-        // Add info to character info.
-
-        // Reset all options for modal.
-
         // Hide the modal.
 		$("#createModal").modal("hide");
 
+        // Values for character creation.
+        var characterName = $("#character-name-input").val();
+        var characterClass = "";
+        var characterGender = "";
+        var characterLevel = 0;
+        var characterMoney = 0;
+
+        // POST the character.
+        $.ajax({
+            type: 'POST',
+            url: "http://lmu-diabolical.appspot.com/characters",
+            data: JSON.stringify({
+                name: characterName,
+                classType: characterClass,
+                gender: characterGender,
+                level: characterLevel,
+                money: characterMoney
+            }),
+            contentType: "application/json",
+            dataType: "json",
+            accept: "application/json",
+            complete: function (jqXHR, textStatus) {
+                // The new character can be accessed from the Location header.
+                console.log("You may access the new character at:" +
+                    jqXHR.getResponseHeader("Location"));
+            }
+        });
+
+        // Update list of characters after creating a new one.
+        $.getJSON(
+            "http://lmu-diabolical.appspot.com/characters",
+            function (characters) {
+                // Do something with the character list.
+                characters.forEach(function (character) {
+                    var $characterRow = $(characterListRowTemplate);
+                    $characterRow.find("td:nth-child(1)").text(character.name);
+                    $("#character-list-table > tbody").append($characterRow);
+                });
+            }
+        );
+
+    });
+
+    // Edit the character.
+    $("#confirm-edit-button").click(function () {
+        // Hide the modal.
+        $("#editModal").modal("hide");
+
+        // Values for character creation.
+        var characterId = 0;
+        var characterName = $("#character-name-input").val();
+        var characterClass = ""; 
+        var characterGender = ""; 
+        var characterLevel = 0; 
+        var characterMoney = 0; 
+        
+        $.ajax({
+            type: 'PUT',
+            url: "http://lmu-diabolical.appspot.com/characters/" + characterId.toString(),
+            data: JSON.stringify({
+                id: characterId,
+                name: characterName,
+                classType: characterClass,
+                gender: characterGender,
+                level: characterLevel,
+                money: characterMoney
+            }),
+            contentType: "application/json",
+            dataType: "json",
+            accept: "application/json",
+            success: function (data, textStatus, jqXHR) {
+                console.log("Done: no news is good news.");
+            }
+        });
+    });
+
+    // Delete the character.
+    $("#confirm-delete-button").click(function () {
+        // Hide the modal
+        $("#deleteModal").modal("hide");
+
+        $.ajax({
+            type: 'DELETE',
+            url: "http://lmu-diabolical.appspot.com/characters/" + "",
+            success: function (data, textStatus, jqXHR) {
+                console.log("Gone baby gone.");
+            }
+        });
     });
 
     // Character Selection
@@ -45,8 +126,7 @@ $(function () {
         // Show the character info column.
         $("#character-info").collapse("show");
 
-        // If you select a cnewharacter, hide the detailed info.
-        $("#detailed-info").collapse("hide");
+        // If you select a new character, hide the enlarge dimage and item list.
         $("#enlarged-image").collapse("hide");
         $("#item-list").collapse("hide");
 
@@ -60,71 +140,23 @@ $(function () {
 
         // Put in new table.
         $.getJSON(
-        "http://lmu-diabolical.appspot.com/characters/4954004257767424",
+        "http://lmu-diabolical.appspot.com/characters/" + "4954004257767424",
         function (character) {
             // Send the character name to the info panel.
             $("#character-name > h3").text(character.name);
 
             // Do something with the character list.
             var $characterInfo = $(characterInfoRowTemplate);
-            $characterInfo.find("tr:eq(1) td:eq(2)").text(character.classType);
-            $characterInfo.find("tr:eq(2) td:eq(2)").text(character.gender);
-            $characterInfo.find("tr:eq(3) td:eq(2)").text(character.level);
-            $characterInfo.find("tr:eq(4) td:eq(2)").text(character.money);
+            console.log(typeof($characterInfo))
+
+            $characterInfo.find("#character-class").text(character.classType);
+            $characterInfo.find("#character-gender").text(character.gender);
+            $characterInfo.find("#character-level").text(character.level);
+            $characterInfo.find("#character-money").text(character.money);
             $("#character-info-table > tbody").html($characterInfo);
         });
 
 
-    });
-
-    // Edit the character.
-    $("#confirm-edit-button").click(function () {
-        // Edit the character's info.
-
-        // Reset options for modal.
-
-        // Hide the modal.
-        $("#editModal").modal("hide");
-    });
-
-    // Delete the character.
-    $("#confirm-delete-button").click(function () {
-        // Delete the character from the table.
-
-        // Hide the modal
-        $("#deleteModal").modal("hide");
-    });
-
-
-    // Enlarge the characters's image.
-    $("#character-title").click(function(){
-        $("#enlarged-image > h2").text($(this).find("#character-name").text())
-
-        // Hide detailed info and item list.
-        $("#detailed-info").collapse("hide");
-        $("#item-list").collapse("hide");
-
-        // Show the character's enlarged image.
-        $("#enlarged-image").collapse("show");
-    });
-
-    $("#test").click(function () {
-        $("#test").popover("show");
-    });
-
-    // Spawn a random item.
-    $("#spawn-item").click(function () {
-        $("#character-items-table").append("<tr><td>Mace</td><td>Weapon</td></tr>")
-    });
-
-    // View the character's items.
-    $("#view-items").click(function () {
-        // Hide detailed info and enlarged picture.
-        $("#detailed-info").collapse("hide");
-        $("#enlarged-image").collapse("hide");
-
-        // Show item list.
-        $("#item-list").collapse("show");
     });
 
     // Set up handlers for character information clicks.
@@ -137,13 +169,38 @@ $(function () {
         // Hide item list and enlarged image.
         $("#item-list").collapse("hide");
         $("#enlarged-image").collapse("hide");
-
-        // Put the currently selected row in the table nfo title.
-        $("#detailed-info > h3").text($(this).find("td:nth-child(1)").text() +  " : " + $(this).find("td:nth-child(2)").text());
-
-        // Show detailed info.
-        $("#detailed-info").collapse("show");
     });
+
+    // Enlarge the characters's image.
+    $("#character-title").click(function(){
+        $("#enlarged-image > h1").text($(this).find("#character-name").text())
+
+        // Hide item list.
+        $("#item-list").collapse("hide");
+
+        // Show the character's enlarged image.
+        $("#enlarged-image").collapse("show");
+    });
+
+    $("#test").click(function () {
+        $("#test").popover("show");
+    });
+
+    // View the character's items.
+    $("#view-items").click(function () {
+        // Hide enlarged picture.
+        $("#enlarged-image").collapse("hide");
+
+        // Show item list.
+        $("#item-list").collapse("show");
+    });
+
+     // Spawn a random item.
+    $("#spawn-item").click(function () {
+        $("#character-items-table").append("<tr><td>Mace</td><td>Weapon</td></tr>")
+    });
+
+
 
     var characterListRowTemplate = '<tr>' +
         '<td></td>'+
