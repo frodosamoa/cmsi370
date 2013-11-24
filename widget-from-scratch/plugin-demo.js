6ca57a150ccac23d2c0e7d0e5bea4f343dea1c44
frodosamoa <andrewrkowalczyk@gmail.com>
2013-11-23 20:40:54 -0800

    added more examples showcasing shapes and colors of switches

--- widget-from-scratch/plugin-demo.js
+++ widget-from-scratch/plugin-demo.js
@@ -6,23 +6,71 @@ $(function () {
         },
 
         shape: "round",
-
-        leftActive: true,
-
-        change: function (oldSelection, newSelection) {
-            console.log("Changed from " + oldSelection + " to " + newSelection);
-        }
+        activeSide : "left",
+        color: "blue",
+        width: 400,
+        height: 100
     });
+
+    $(".drag-select-onoff").dragSelect({
+        values : {
+            left: "off",
+            right: "on"
+        },
+
+        color: "dark",
+        width: "100%"
+    });
+
 
     $(".drag-select-yesno").dragSelect({
         values : {
-            left: "Yes",
+            left: "yes",
             right: "no"
         },
 
-        change: function (oldSelection, newSelection) {
-            console.log("Changed from " + oldSelection + " to " + newSelection);
-        }
+        activeSide: "right",
+        shape: "round",
+        color: "purple"
+    });
+
+    $(".drag-select-mf").dragSelect({
+        values : {
+            left: "M",
+            right: "F"
+        },
+
+        width: 80,
+        height: 40,
+        activeSide: "right",
+        shape: "round",
+        color: "green"
+    });
+
+    $(".drag-select-OI").dragSelect({
+        values : {
+            left: "O",
+            right: "I"
+        },
+
+        width: 80,
+        height: 40,
+        activeSide: "right",
+        shape: "round",
+        color: "light"
+    });
+
+    $(".drag-select-MALEFEMALE").dragSelect({
+        values : {
+            left: "MALE",
+            right: "FEMALE"
+        },
+
+        width: 300,
+        height: 70,
+        activeSide: "right",
+        shape: "round",
+        color: "red"
     });
 
 });
