5c6900d2bf499796e6d95f5a1b9623ec34343889
frodosamoa <andrewrkowalczyk@gmail.com>
2013-11-23 20:40:26 -0800

    removed now not useful css styles, implemented colors for the switches

--- widget-from-scratch/jquery.drag-select.js
+++ widget-from-scratch/jquery.drag-select.js
@@ -20,13 +20,49 @@
     -the default is 150 if no width is assigned.
 
 */
-(function ($) {
 
-    // Templates for the right and left values of the switch.
-    var $leftActiveTemplate = $("<p class=\"leftActive\"></p>"),
-        $rightActiveTemplate = $("<p class=\"rightActive\"></p>"),
-        $leftUnactiveTemplate = $("<p class=\"leftUnactive\"></p>"),
-        $rightUnactiveTemplate = $("<p class=\"rightUnactive\"></p>")
+(function ($) {
+    // Default values stored as "constants".
+    var DEFAULT_WIDTH = 40,
+        DEFAULT_HEIGHT = 150,
+        DEFAULT_SHAPE = "square",
+        DEFAULT_COLOR = "white",
+
+        // Color values.
+        BLUE = {
+            background : "#1589FF",
+            switcher   : "#82CAFF",
+            font       : "#1029CC"
+        },
+        RED = {
+            background : "#AD0C2F",
+            switcher : "#ED3B62",
+            font : "#610B23"
+        }
+        GREEN = {
+            background : "#1A871D",
+            switcher : "#24D62A",
+            font : "#0B610E"
+        }
+        PURPLE = {
+            background : "#4D1C9C",
+            switcher : "#875CCC",
+            font : "#A87ACF"
+        }
+        LIGHT = {
+            background : "#D0D0D0",
+            switcher : "#FFFFFF",
+            font : "#454545"
+        }
+        DARK = {
+            background : "#505050",
+            switcher : "#808080",
+            font : "#C7C7C7"
+        }
+
+        // Templates for the right and left values of the switch.
+        $leftValueTemplate = $("<p class=\"left\"></p>"),
+        $rightValueTemplate = $("<p class=\"right\"></p>"),
         $switcher = $("<div class=\"switcher\"></div>");
 
     // Private plugin helpers.
@@ -45,52 +81,97 @@
             leftInitialActiveSide = options.activeSide ? options.activeSide === "left" : true,
 
             // Shape of the switch.
-            shape = options.shape ? options.shape : "square",
+            shape = options.shape ? options.shape : DEFAULT_SHAPE,
+
+            // Color of the switch.
+            color = options.color ? options.color : DEFAULT_COLOR,
+            backgroundColor,
+            switchColor,
+            fontColor,
 
             // Height and width of our switch.
-            height = options.height ? options.height : 40,
-            width = options.width ? options.width: 150,
+            height = options.height ? options.height : DEFAULT_WIDTH,
+            width = options.width ? options.width: DEFAULT_HEIGHT,
 
             // Using templates to create clone of each div or p.
-            $leftActiveField = $leftActiveTemplate.clone(),
-            $rightActiveField = $rightActiveTemplate.clone()
-            $leftUnactiveField = $leftUnactiveTemplate.clone(),
-            $rightUnactiveField = $rightUnactiveTemplate.clone();
+            $leftValue = $leftValueTemplate.clone(),
+            $rightValue = $rightValueTemplate.clone()
             $switcherClone = $switcher.clone();
 
-        // Set the width, height, and shape for the drag-select.
-        $this.css("width", width);
-        $this.css("height", height);
-        $this.css("border-radius", shape === "round" ? (height / 2) : 3);
+
+        switch (color) {
+
+            case "red":
+                backgroundColor = RED.background;
+                switchColor = RED.switcher;
+                fontColor = RED.font;
+            break;
+
+            case "blue":
+                backgroundColor = BLUE.background;
+                switchColor = BLUE.switcher;
+                fontColor = BLUE.font;
+            break;
+
+            case "green":
+                backgroundColor = GREEN.background;
+                switchColor = GREEN.switcher;
+                fontColor = GREEN.font;
+            break;
+
+            case "purple":
+                backgroundColor = PURPLE.background;
+                switchColor = PURPLE.switcher;
+                fontColor = PURPLE.font;
+            break;
+
+            case "light":
+                backgroundColor = LIGHT.background;
+                switchColor = LIGHT.switcher;
+                fontColor = LIGHT.font;
+            break;
+
+            case "dark":
+                backgroundColor = DARK.background;
+                switchColor = DARK.switcher;
+                fontColor = DARK.font;
+            break;
+        }
+
+        // Set the width, height, shape, and color for the drag-select.
+        $this
+            .css("width", width).css("height", height)
+            .css("border-radius", shape === "round" ? (height / 2) : 3)
+            .css("background", backgroundColor)
+            .css("border-color", backgroundColor);
+
 
         // Put in the values into the div templates.
-        $leftActiveField.text(leftValue);
-        $rightActiveField.text(rightValue);
-        $leftUnactiveField.text(leftValue);
-        $rightUnactiveField.text(rightValue);
+        $leftValue.text(leftValue);
+        $rightValue.text(rightValue);
 
         // Append the right value, left value, and the selector to our drag select div.
         $this.addClass("drag-select")
-            .append($leftActiveField, $leftUnactiveField,
-                    $rightActiveField, $rightUnactiveField,
-                    $switcherClone);
+            .append($leftValue, $rightValue, $switcherClone);
 
         // Values used for centering values vertically and horizontally.
         var innerHeight = $this.innerHeight(),
             innerWidth = $this.innerWidth(),
-            centerValueMargin = -($this.find(".leftActive").height() / 2),
+            centerValueMargin = -($this.find(".left").height() / 2),
             topBottomPadding = parseInt($this.css("padding-top")),
             leftPadding = parseInt($this.css("padding-left")),
             rightPadding = parseInt($this.css("padding-right")),
             fourthInnerWidth = (innerWidth / 4);
 
         // Center the right and left values vertically. 
-        $this.find(".leftActive, .leftUnactive")
+        $this.find(".left")
             .css("margin-top", centerValueMargin)
-            .css("left", fourthInnerWidth + centerValueMargin);
-        $this.find(".rightActive, .rightUnactive")
+            .css("left", fourthInnerWidth - ($this.find(".left").width() / 2))
+            .css("color", fontColor);
+        $this.find(".right")
             .css("margin-top", centerValueMargin)
-            .css("right", fourthInnerWidth + centerValueMargin);
+            .css("right", fourthInnerWidth - ($this.find(".right").width() / 2))
+            .css("color", fontColor);
 
         // Make the switch be as close as possible to the edges and set it to the default value.
         $this.find(".switcher")
@@ -98,9 +179,11 @@
             .css("width", ((innerWidth - leftPadding - rightPadding)/ 2))
             .css("left", leftInitialActiveSide ? leftPadding : "auto")
             .css("right", leftInitialActiveSide ? "auto" : rightPadding)
-            .css("border-radius", shape === "round" ? (height / 2) : 3);
+            .css("border-radius", shape === "round" ? (height / 2) : 3)
+            .css("background", switchColor)
+            .css("border-color", switchColor);
 
-        $this.click(function () {
+        $this.click(function() {
             var switchClicked = $this.find(".switcher"),
                 leftCSS = switchClicked.css("left"),
                 rightCSS = switchClicked.css("right"),
@@ -109,15 +192,15 @@
 
             switchClicked
                 .css("left", leftActive ? "auto" : leftPadding)
-                .css("right", rightActive ? "auto" : rightPadding);
-        });
+                .css("right", rightActive ? "auto" : leftPadding);
+        })
 
         $this.find(".switcher")
             .mousedown(function (event) {
-                $current = $(this);
-                anchorX = event.pageX;
+                $current = $this.find(".switcher");
                 rightClicked = $current.css("left") === "auto";
-            });
+                anchorX = event.pageX;
+            })
 
         $(document)
             .mousemove(function (event) {
@@ -145,22 +228,22 @@
                 if ($current) {
                     var leftCSS = $current.css("left"),
                         rightCSS = $current.css("right"),
-                        leftGreater;
+                        snapSide;
 
                     if (leftCSS === "auto") {
-                        leftGreater = false;
+                        snapSide = false;
                     } else if (rightCSS === "auto") {
-                        leftGreater = true;
+                        snapSide = true;
                     } else {
-                        leftGreater = parseInt(leftCSS) > parseInt(rightCSS);
+                        snapSide = parseInt(leftCSS) > parseInt(rightCSS);
                     }
 
-                    $current.css("right", leftGreater ? "auto" : rightPadding)
-                        .css("left", leftGreater ? leftPadding : "auto");
+                    $current.css("right", snapSide ? "auto" : rightPadding)
+                        .css("left", snapSide ? leftPadding : "auto");
 
-                    // Reset anchorX, current, and rightClicked.
-                    rightClicked = false;
+                    // Reset current and rightClicked.
                     anchorX = 0;
+                    rightClicked = false;
                     $current = null;
                 }
             });
